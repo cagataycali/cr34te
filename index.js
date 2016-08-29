e9335c37f121294699f16dc0b41ea73ee8f78a73
cagataycali <calicagatay@icloud.com>
2016-08-29 08:02:10 -0700

    Description

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var B = require('br4nch');
 var E = require('3x3c');
+var emoji = require('node-emoji');
+var colors = require('colors');
 var async = require('async');
 var updateNotifier = require('update-notifier');
 var pkg = require('./package.json');
@@ -25,7 +27,11 @@ function R(cmd, username, repo) {
 
 module.exports = function C() {
  return new Promise(function(resolve, reject) {
-   var prompt = require('prompt');
+       var prompt = require('prompt');
+       console.log(colors.green('Be sure about your \n'));
+       console.log(emoji.emojify(' :rotating_light: '), emoji.emojify(colors.green(` Github username already set in your git config: `)), colors.inverse('git config --global github.user <username>'));
+       console.log(emoji.emojify(' :rotating_light: '), emoji.emojify(colors.green(` SSH keys registered in github: `)), colors.inverse('https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/'));
+
        prompt.start();
        prompt.get([{name:'repo', required: true, description: "Github repo name: Ex. MyAwesomeRepo"}], function (err, result) {
          E(`git config user.name`) // Be sure your git.config.username equal your github username
@@ -34,7 +40,6 @@ module.exports = function C() {
                .then((value) => {
                  resolve(`https://github.com/${username}/${result.repo}.git`);
                })
-              //  .catch((err) => {reject(err)});
            })
            .catch((err) => {reject(err)})
        });
